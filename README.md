41db34e729a26770bd6e66b1a870c49635353136
nkuang123 <nkuang@vassar.edu>
2019-09-21 17:02:47 -0400

    added vzip

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Operating Systems
+
+This repository holds all of my assignments and labs for Vassar College's Operating Systems course.
--- assn1/vzip.c
+++ assn1/vzip.c
@@ -9,10 +9,39 @@ int main(int argc, char *argv[]) {
 
 	if (argc < 2) {
 		printf("vzip: file1 [file2 ...]\n");
-		exit(1);
-	}
+		exit(1);	}
 
 	char *file_name = argv[1];
+
+	FILE *stream = fopen(file_name, "r");
+	FILE *write_ptr = fopen("test.bin","wb");
+
+	char ch;
+	char next;
+	int count = 0;
+
+	while ((ch = fgetc(stream)) != EOF) {
+
+		count++;
+
+		if ((next = fgetc(stream)) != ch) {
+			ungetc(next, stream);
+
+			fwrite(&count, sizeof(int), 1, write_ptr);
+			fwrite(&ch, 1, sizeof(ch), write_ptr);
+			printf("%x%c", count, ch);
+
+
+			count = 0;
+		} else {
+			ungetc(next, stream);
+		}
+	}
+
+	printf("\n");
+
+	fclose(stream);
+	fclose(write_ptr);
 
 
 
